6b55b1de5e0bea75b3ad367f064327dbafc3fe95
Sparsh Kumar <SpKumar@sv.us.sonicwall.com>
2017-06-13 15:50:49 +0530

    Work Today

--- practice1/src/app/app.component.ts
+++ practice1/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/Rx';
+import { jsonModel } from './json.model';
+import { ArticleService } from './article.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./style.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  articles: jsonModel[];
+  errorMessage: string;
+  @Input() l: string;
+
+  constructor(private _articleService: ArticleService) {
+    this.articles = [];
+    this.l = "loading";
+  }
+
+  ngOnInit():void {
+    let self = this;
+    self._articleService.getArticles().subscribe(response => this.articles = response, error => this.errorMessage = < any > error, () => this.completed())
+  }
+
+  completed() {
+    this.l="random";
+  }
 }
--- practice1/src/app/app.module.ts
+++ practice1/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdProgressSpinnerModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ArticleService } from './article.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MdProgressSpinnerModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
