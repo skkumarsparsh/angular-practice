28c7f4f5fbc2c0d82417aa7a16432127f879dbab
Sparsh Kumar <SpKumar@sonicwall.com>
2017-07-12 09:54:35 +0530

    completed infinite scroll

--- practice4/src/app/app.module.ts
+++ practice4/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AgGridModule} from 'ag-grid-angular/main';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    JsonpModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
