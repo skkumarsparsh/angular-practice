1d1a39182849b61cbedec177429fe1b4b183862e
Sparsh Kumar <SpKumar@sonicwall.com>
2017-07-17 13:24:23 +0530

    half working

--- loadingbar/src/app/app.component.ts
+++ loadingbar/src/app/app.component.ts
@@ -9,4 +9,22 @@ export class AppComponent {
 
   constructor() {
   }
+
+  ngOnInit() {
+    let x = 300;
+    this.loopStuffDown(x);
+  }
+
+  loopStuffDown(x){
+
+    var i = x;
+    (function loop(){
+      if (i-- < 1) return;
+      setTimeout(function(){
+        document.getElementById("child").style.width = i + "px";
+        loop();
+      }, 10);
+    })();
+  }
+
 }
