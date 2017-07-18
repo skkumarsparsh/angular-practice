8f03eabbf5cc7ea7c69d478d8024a19eee8f005e
Sparsh Kumar <SpKumar@sonicwall.com>
2017-07-18 11:29:55 +0530

    animation done without looping

--- loadingbar/src/app/app.component.ts
+++ loadingbar/src/app/app.component.ts
@@ -13,16 +13,32 @@ export class AppComponent {
   ngOnInit() {
     let x = 300;
     this.loopStuffDown(x);
-    console.log(x);
   }
 
   loopStuffDown(x){
 
     var i = x;
+    var k=0;
     (function loop(){
-      if (i-- < 1) return;
+      if (i-- < 1) 
+      {
+        i=0;
+        k=299;
+        (function loop2(){
+          if(i++ > 299) return;
+          setTimeout(function(){
+            document.getElementById("child").style.width = i + "px";
+            document.getElementById("child").style.marginLeft = k + "px";
+            k--;
+            loop2();
+          }, 5);
+        })();
+        return;
+      }
       setTimeout(function(){
         document.getElementById("child").style.width = i + "px";
+        document.getElementById("child").style.marginLeft = k + "px";
+        k++;
         loop();
       }, 5);
     })();
