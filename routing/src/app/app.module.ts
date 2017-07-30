7c921cef5f40a6750a91dd4f23b00364e62455fb
Sparsh <skkumarsparsh@gmail.com>
2017-07-30 21:11:14 +0530

    routing changes

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
+import { routes as childRoutes,ProductsModule } from './products/products.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
@@ -9,6 +10,12 @@ import { ContactComponent } from './contact/contact.component';
 import {MdSidenavModule} from '@angular/material';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { ProductsComponent } from './products/products.component';
+
+import { AUTH_PROVIDERS } from './auth.service';
+import { LoggedInGuard } from './logged-in.guard';
 
 const routes: Routes = [
   // basic routes
@@ -17,6 +24,9 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'contactus', redirectTo: 'contact' },
+  { path: 'login', component: LoginComponent },
+  { path: 'protected',component: ProtectedComponent,canActivate: [ LoggedInGuard ] },
+  { path: 'products',component: ProductsComponent,children: childRoutes }
 ]
 
 @NgModule({
@@ -24,15 +34,21 @@ const routes: Routes = [
     AppComponent,
     HomeComponentComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdSidenavModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ProductsModule
   ],
-  providers: [],
+  providers: [
+    AUTH_PROVIDERS,
+    LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
