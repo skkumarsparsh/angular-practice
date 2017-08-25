af2eda7a9043eb7f7f5bd50911b84f40c761d6bc
Sparsh <skkumarsparsh@gmail.com>
2017-08-25 21:13:12 +0530

    Some miscellaneous changes

--- hackathon-practice2.0/src/app/protected/home/home.component.ts
+++ hackathon-practice2.0/src/app/protected/home/home.component.ts
@@ -40,7 +40,6 @@ export class HomeComponent implements OnInit {
       let that = this;
       setTimeout(function () {
         that._service.success("Hi, Lead Agent! Welcome back!");
-        // this.utils.notificationAdded.emit(["Hi, Lead Agent! Welcome back!", "green", "check_circle"])
       }, 200);
       this.utils.firstLoad3 = false;
     }
@@ -68,33 +67,33 @@ export class HomeComponent implements OnInit {
     this.metricname1 = core[0];
     this.metricname2 = core[1];
     this.metricname3 = core[2];
-    let lar = this.data[lead][core[0]][months[0]]
-    this.metricmonth2 = months[0];
-    this.metric2 = lar;
+    let lar = this.data[lead][core[0]][months[0]] || "0";
+    this.metricmonth1 = months[0];
+    this.metric1 = lar;
     for(var i=1;i<3;i++) {
-      let val = this.data[lead][core[0]][months[i]];
+      let val = this.data[lead][core[0]][months[i]] || "0";
       if(val > lar){
         lar = val;
         this.metricmonth1 = months[i];
         this.metric1 = val;
       }
     }
-    lar = this.data[lead][core[1]][months[0]]
+    lar = this.data[lead][core[1]][months[0]] || "0"
     this.metricmonth2 = months[0];
     this.metric2 = lar;
     for(var i=1;i<3;i++) {
-      let val = this.data[lead][core[1]][months[i]];
+      let val = this.data[lead][core[1]][months[i]] || "0";
       if(val > lar){
         lar = val;
         this.metricmonth2 = months[i];
         this.metric2 = val;
       }
     }
-    lar = this.data[lead][core[2]][months[0]]
+    lar = this.data[lead][core[2]][months[0]] || "0"
     this.metricmonth3 = months[0];
     this.metric3 = lar;
     for(var i=1;i<3;i++) {
-      let val = this.data[lead][core[2]][months[i]];
+      let val = this.data[lead][core[2]][months[i]] || "0";
       if(val > lar){
         lar = val;
         this.metricmonth3 = months[i];
@@ -102,11 +101,11 @@ export class HomeComponent implements OnInit {
       }
     }
     this.metricname4 = "Sales Value";
-    lar = this.data[lead]["Sales Value"][months[0]];
+    lar = this.data[lead]["Sales Value"][months[0]] || "0";
     this.metricmonth4 = months[0];
     this.metric4 = lar;
     for(var i=1;i<3;i++) {
-      let val = this.data[lead]["Sales Value"][months[i]];
+      let val = this.data[lead]["Sales Value"][months[i]] || "0";
       if(val > lar) {
         lar = val;
         this.metricmonth4 = months[i];
