2e98bfdd3049f158829ee9ba364f44e63026a11e
Sparsh <skkumarsparsh@gmail.com>
2017-09-03 14:03:38 +0530

    Profile page dark mode complete

--- hackathon-practice2.0/src/app/protected/profile/profile.component.ts
+++ hackathon-practice2.0/src/app/protected/profile/profile.component.ts
@@ -23,13 +23,18 @@ export class ProfileComponent implements OnInit {
   }
 
   loading(res) {
-    if(document.getElementById("left") && document.getElementById("right")) {
+    if(document.getElementById("left") && document.getElementById("right") && document.getElementById("lefttext") &&
+      document.getElementById("name")) {
       if(res==true) {
         document.getElementById("left").setAttribute("style","background-color:rgb(62,62,62);");
         document.getElementById("right").setAttribute("style","background-color:rgb(51,51,51);");
+        document.getElementById("lefttext").setAttribute("style","color:white;");
+        document.getElementById("name").setAttribute("style","color:white;");
       } else {
         document.getElementById("left").setAttribute("style","background:rgba(255,255,255,1);");
         document.getElementById("right").setAttribute("style","background:rgba(245,245,245,1);");
+        document.getElementById("lefttext").setAttribute("style","rgba(140,140,140,1);");
+        document.getElementById("name").setAttribute("style","color:rgba(30,30,30,1);");
       }
     }
   }
