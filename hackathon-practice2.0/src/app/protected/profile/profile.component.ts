2bb0cb158a69725db70af2bcb0a1383435e81beb
Sparsh <skkumarsparsh@gmail.com>
2017-09-03 13:15:45 +0530

    Changes to profile screen in dark mode

--- hackathon-practice2.0/src/app/protected/profile/profile.component.ts
+++ hackathon-practice2.0/src/app/protected/profile/profile.component.ts
@@ -15,14 +15,23 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     this.utils.slidetoggle.subscribe(res => {
-      if(document.getElementById("card")) {
-        if(res==true) {
-          document.getElementById("card").setAttribute("style","background-color:rgb(51,51,51);");
-        } else {
-          document.getElementById("card").setAttribute("style","background:rgba(255,255,255,1);");
-        }
-      }
+      this.loading(res);
     })
+    setTimeout(()=>{
+      this.loading(this.utils.checked);
+    },5);
+  }
+
+  loading(res) {
+    if(document.getElementById("left") && document.getElementById("right")) {
+      if(res==true) {
+        document.getElementById("left").setAttribute("style","background-color:rgb(62,62,62);");
+        document.getElementById("right").setAttribute("style","background-color:rgb(51,51,51);");
+      } else {
+        document.getElementById("left").setAttribute("style","background:rgba(255,255,255,1);");
+        document.getElementById("right").setAttribute("style","background:rgba(245,245,245,1);");
+      }
+    }
   }
 
 }
